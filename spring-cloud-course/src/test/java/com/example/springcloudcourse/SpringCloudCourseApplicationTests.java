54c3ce4bdbbc92ceadce8c01cf4e92a635573e82
Zane47 <1228073191@qq.com>
2022-01-08 16:22:57 +0800

    spring cloud 101

---
+++ spring-cloud-course/src/test/java/com/example/springcloudcourse/SpringCloudCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
